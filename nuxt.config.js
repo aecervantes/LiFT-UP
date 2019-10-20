3835606caa217f32e2055b2f9902efc477d80745
Tyler Patton <jamestylerpatton@gmail.com>
2019-10-19 18:11:45 -0700

    Move bootstrap scripts to end body

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,3 @@
-
 export default {
   mode: 'universal',
   /*
@@ -13,6 +12,26 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      {
+        src: 'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+        integrity: 'sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN',
+        crossorigin: 'anonymous',
+        body: true
+      },
+      {
+        src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
+        integrity: 'sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q',
+        crossorigin: 'anonymous',
+        body: true
+      },
+      {
+        src: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js',
+        integrity: 'sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl',
+        crossorigin: 'anonymous',
+        body: true
+      },
     ]
   },
   /*
@@ -23,7 +42,7 @@ export default {
   ** Global CSS
   */
   css: [
-  "~/assets/css/style.css"
+  "~/assets/css/style.css",
   ],
   /*
   ** Plugins to load before mounting the App
